dd6c545de8efcc92f21afd888d0e88f4ae552b9e
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-09-17 11:16:08 +0700

    Mengatasi Error CORS ORIGIN pada API NodeJS

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 const productRoutes = require('./src/routes/products');
 app.use(bodyParser.json()); // type JSON
 
+app.use((req, res, next)=> {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT, OPTION');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 app.use('/v1/customer', productRoutes);
 
