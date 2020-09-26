4ba8fdbd90e209b145274f43d2cb97e01f2ae76d
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-09-26 09:48:52 +0700

    Menkoneksi kan server MongoDB ke server Node JS

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const authRoutes = require('./src/routes/auth');
 const blogsRoutes = require('./src/routes/blogs');
 app.use(bodyParser.json()); // type JSON
 
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT, OPTION');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -21,8 +22,13 @@ app.use((error, req, res, next) => {
     const status = error.errorStatus || 500;
     const message = error.message;
     const data = error.data;
-    res.status(status).json({ message: message, data: data})
+    res.status(status).json({ message: message, data: data })
 })
 
-app.listen(4000);
+mongoose.connect('mongodb+srv://ariyaagustian:sarafgagap@cluster-1.14ed2.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(4000, () => console.log('Connection Success'));
+    })
+    .catch(err => console.log(err));
+
 
