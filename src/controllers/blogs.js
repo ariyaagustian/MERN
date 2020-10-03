9ca147dc7fddd23095ce68efe362e9233edf9a14
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-10-03 11:10:47 +0700

    Mengambil Seluruh Data Postingan Blog (Cara melakukan GET pada NodeJS)

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,18 +1,18 @@
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 const BlogPost = require('../models/blogs');
 
 exports.createPost = (req, res, next) => {
-    
+
 
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         const err = new Error('Input Value Tidak Sesuai');
         err.errorStatus = 400;
         err.data = errors.array();
         throw err;
     } else {
 
-        if(!req.file){
+        if (!req.file) {
             const err = new Error('Image Harus Di Upload');
             err.errorStatus = 422;
             err.data = errors.array();
@@ -27,63 +27,40 @@ exports.createPost = (req, res, next) => {
             title: title,
             body: body,
             image: image,
-            author: {uid: 1, name: "Ariya Agustian"}
+            author: { uid: 1, name: "Ariya Agustian" }
         });
 
         Posting.save()
-        .then(result => {
-            res.status(201).json({
-                message : "Create Blog Post Success",
-                data : result
+            .then(result => {
+                res.status(201).json({
+                    message: "Create Blog Post Success",
+                    data: result
+                });
+            })
+            .catch(err => {
+                console.log('err : ', err);
             });
-        })
-        .catch(err =>  {
-            console.log('err : ', err);
-        });        
     }
 }
 
 exports.getAllPosts = (req, res, next) => {
-
-    const result = {
-        message: "Get All Blog Post Success",
-        data: [
-            {
-                id: 1,
-                title: "Anim non aliqua nulla duis ex sit nulla amet ipsum ullamco.",
-                body: "Id deserunt eu dolor pariatur pariatur labore enim et officia. Laboris dolore nostrud qui adipisicing esse. Sunt eiusmod eu mollit ullamco eu. Voluptate dolore enim fugiat pariatur et consequat irure consectetur sit officia mollit ut nulla. Fugiat voluptate fugiat eiusmod ipsum Lorem esse laboris nisi anim in."
-            },
-            {
-                id: 2,
-                title: "Anim non aliqua nulla duis ex sit nulla amet ipsum ullamco.",
-                body: "Id deserunt eu dolor pariatur pariatur labore enim et officia. Laboris dolore nostrud qui adipisicing esse. Sunt eiusmod eu mollit ullamco eu. Voluptate dolore enim fugiat pariatur et consequat irure consectetur sit officia mollit ut nulla. Fugiat voluptate fugiat eiusmod ipsum Lorem esse laboris nisi anim in."
-            },
-            {
-                id: 3,
-                title: "Anim non aliqua nulla duis ex sit nulla amet ipsum ullamco.",
-                body: "Id deserunt eu dolor pariatur pariatur labore enim et officia. Laboris dolore nostrud qui adipisicing esse. Sunt eiusmod eu mollit ullamco eu. Voluptate dolore enim fugiat pariatur et consequat irure consectetur sit officia mollit ut nulla. Fugiat voluptate fugiat eiusmod ipsum Lorem esse laboris nisi anim in."
-            },
-            {
-                id: 4,
-                title: "Anim non aliqua nulla duis ex sit nulla amet ipsum ullamco.",
-                body: "Id deserunt eu dolor pariatur pariatur labore enim et officia. Laboris dolore nostrud qui adipisicing esse. Sunt eiusmod eu mollit ullamco eu. Voluptate dolore enim fugiat pariatur et consequat irure consectetur sit officia mollit ut nulla. Fugiat voluptate fugiat eiusmod ipsum Lorem esse laboris nisi anim in."
-            },
-            {
-                id: 5,
-                title: "Anim non aliqua nulla duis ex sit nulla amet ipsum ullamco.",
-                body: "Id deserunt eu dolor pariatur pariatur labore enim et officia. Laboris dolore nostrud qui adipisicing esse. Sunt eiusmod eu mollit ullamco eu. Voluptate dolore enim fugiat pariatur et consequat irure consectetur sit officia mollit ut nulla. Fugiat voluptate fugiat eiusmod ipsum Lorem esse laboris nisi anim in."
-            },
-        ]
-    }
-
-    res.status(200).json(result);
+    BlogPost.find()
+        .then(result => {
+            res.status(200).json({
+                message: 'Data Blog Post Berhasil Dipanggil',
+                data: result
+            });
+        })
+        .catch(err => {
+            next(err);
+        })
 }
 
 exports.deletePost = (req, res, next) => {
     const title = req.body.title;
     const body = req.body.body;
     const id = req.params.id;
-    
+
     const result = {
         message: "Delete Post Success!",
         data: {
