4a3ff30eb1ff698f8060f41a310148e6a97c56f1
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-11-02 08:41:28 +0700

    43 - Mengganti data dummy di home page dengan response API

--- src/components/molecules/BlogItem/index.js
+++ src/components/molecules/BlogItem/index.js
@@ -4,15 +4,17 @@ import { Button, Gap } from '../../atoms'
 import './blogItem.scss'
 import {useHistory} from 'react-router-dom'
 
-const BlogItem = () => {
+const BlogItem = (props) => {
     const history = useHistory();
+    const {image, title, name, date, body} = props;
+
     return (
         <div className="blog-item">
-            <img className="image-thumb" src={RegisterBg} alt="post"></img>
+            <img className="image-thumb" src={image} alt="post"></img>
             <div className="content-detail">
-                <p className="title">Title blog</p>
-                <p className="author">Author - Date post</p>
-                <p className="body">Eiusmod culpa veniam magna Lorem pariatur exercitation eiusmod proident ad Lorem id voluptate. Quis elit nostrud nisi elit consequat ullamco nostrud eu culpa commodo sunt elit ad fugiat. In culpa veniam ex proident duis irure amet cupidatat duis quis ex pariatur in. Proident dolore nostrud et sint sint adipisicing. Duis tempor veniam irure culpa magna excepteur exercitation laborum amet nisi nostrud consectetur laborum. Sint nostrud dolore ad sit.</p>
+                <p className="title">{title}</p>
+                <p className="author">{name} - {date}</p>
+                <p className="body">{body}</p>
                 <Gap height={20}></Gap>
                 <Button title="View Detail" onClick={() => history.push('/detail-blog')}></Button>
             </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BlogItem, Button, Gap } from '../../components'
 import './home.scss'
 import {useHistory} from 'react-router-dom'
@@ -6,10 +6,14 @@ import Axios from 'axios'
 
 
 const Home = () => {
+    const [dataBlog, setDataBlog] = useState([]);
     useEffect(() => {
         Axios.get('http://localhost:4000/v1/blog/posts')
         .then(result => {
             console.log('Data API', result.data)
+            const responseAPI = result.data;
+
+            setDataBlog(responseAPI.data);
         })
         .catch(err => {
             console.log('error', err)
@@ -23,10 +27,15 @@ const Home = () => {
             </div>
             <Gap height={20}></Gap>
             <div className="content-wrapper">
-                <BlogItem></BlogItem>
-                <BlogItem></BlogItem>
-                <BlogItem></BlogItem>
-                <BlogItem></BlogItem>
+                {dataBlog.map(blog => {
+                    return <BlogItem 
+                        key={blog._id} 
+                        image={`http://localhost:4000/${blog.image}`} 
+                        title={blog.title} 
+                        body={blog.body}
+                        name={blog.author.name}
+                        date={blog.createdAt} />
+                })}
             </div>
             <div className="pagination">
                 <Button title="Previous"></Button>
